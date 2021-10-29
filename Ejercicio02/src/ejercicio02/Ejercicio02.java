9eb9dd759532fb703853e82c2358f84c735c158f
JeraldynDelgado <jmdelgado15@utpl.edu.ec>
2021-10-29 11:58:13 -0500

    primer commit

---
+++ Ejercicio02/src/ejercicio02/Ejercicio02.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio02;
+
+/**
+ *
+ * @author jmdelgado15
+ */
+public class Ejercicio02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Resolucion del problema
+        int var1= 10;
+        int var2= 2;
+        
+        double resultado = (Math.pow(var1, var2))+ var1;
+        
+        System.out.println(resultado);
+    }
+   
+    
+}
---
+++ Ejercicio03/src/ejercicio03/Ejercicio03.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio03;
+
+/**
+ *
+ * @author jmdelgado15
+ */
+public class Ejercicio03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Resolucion del problema
+        int var1 = 10;
+        int var2 = 2;
+        int var3 = 16;
+        int var4 = 20;
+        int var5 = 1;
+        int var6 = 11;
+
+        double resultado = (Math.pow(var1, var2)) + Math.sqrt(var3)
+                + (var4 / (var5 + var6));
+
+        System.out.println(resultado);
+
+    }
+
+}
